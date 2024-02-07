2049fab8e9a6d64ef1d849e04250245d56ec0d07
GlassVN <158665720+GlassVN@users.noreply.github.com>
2024-02-07 12:13:29 +0700

    Delete Exercise directory

--- Exercise/lesson 2.py
+++
@@ -1,8 +0,0 @@
-'''[cho biết số thuộc hàng nào]'''
-num = int(input("nhập ba chữ số:"))
-a = num//100
-b = (num-100*a)//10
-c = (num-100*a-10*b)
-print('hàng trăm', a)
-print('hàng chục', b)
-print('đơn vị', c)
--- Exercise/lesson 8.py
+++
@@ -1,7 +0,0 @@
-'''[bài 6: tính chuyển đổi nhiệt độ]'''
-a = float(input('°C chuyển sang °F \n Nhập °C ='))
-b = 1.8*a+32
-print('°F là:',b)
-c = float(input('°F chuyển sang °C \n Nhập °F ='))
-d = (c-32)/1.8
-print('°C là:',d)
